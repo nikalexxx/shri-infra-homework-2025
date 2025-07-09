cbe0e88a323cefd6c04c515e9e714dcee2e8f956
Vlashex <rmaynavine@gmail.com>
2025-07-09 16:14:45 +0300

    Тест pr.yml

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -11,7 +11,7 @@ export const About: React.FC = () => {
       <div className="row">
         <div className="col">
           <h1>About</h1>
-          <p>Author: Жадан Владислав Андреевич</p>
+          <p>Author: Жадан Владислав</p>
         </div>
       </div>
     </div>
