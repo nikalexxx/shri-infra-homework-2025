89fe4716ebc8998fc115ca54b04c1a1bd4940a38
maryyak <akovenkomaria557@gmail.com>
2025-07-12 00:43:06 +0300

    коммит

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -3,7 +3,7 @@ import { Helmet } from "react-helmet";
 import { cn } from "@bem-react/classname";
 
 const bem = cn("About");
-//Исправлено имя автора
+
 export const About: React.FC = () => {
   return (
     <div className={bem()}>
