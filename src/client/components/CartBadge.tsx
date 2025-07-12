bf61a51c733fb1e706078ed0a5088163bec6c9ea
Dinara Dulanova <dinara.dulanova@yandex.ru>
2025-07-12 22:00:31 +0300

    test: ci/setup

--- src/client/components/CartBadge.tsx
+++ src/client/components/CartBadge.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import { cn } from '@bem-react/classname';
-import { useSelector } from 'react-redux';
-import { ApplicationState } from '../store';
+import React from "react";
+import { cn } from "@bem-react/classname";
+import { useSelector } from "react-redux";
+import { ApplicationState } from "../store";
 
 export interface CartBadgeProps {
-    id: number;
+  id: number;
 }
 
-const bem = cn('CartBadge');
+const bem = cn("CartBadge");
 
 export const CartBadge: React.FC<CartBadgeProps> = ({ id }) => {
-    const cart = useSelector((s: ApplicationState) => s.cart);
+  //test
+  const cart = useSelector((s: ApplicationState) => s.cart);
 
-    return cart[id] ? <span className={bem(null, ['text-success', 'mx-3'])}>Item in cart</span> : null;
-}
+  return cart[id] ? (
+    <span className={bem(null, ["text-success", "mx-3"])}>Item in cart</span>
+  ) : null;
+};
