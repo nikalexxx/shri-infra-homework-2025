0f260a74fb3083d90f101a6c03262a71970b9803
Bronina A.O <bronina04@mail.ru>
2025-07-13 20:34:52 +0300

    another commit

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -11,7 +11,7 @@ export const About: React.FC = () => {
       <div className="row">
         <div className="col">
           <h1>About</h1>
-          <p>Author: Anasteisha</p>
+          <p>Author: test</p>
         </div>
       </div>
     </div>
