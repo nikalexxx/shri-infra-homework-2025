707667341ad88514bcaeda10dee2b37ebc92923e
Анастасия <71327963+anasteisha-4@users.noreply.github.com>
2025-07-13 20:15:46 +0300

    Test (#4)
    * test workflows (test failed)
    
    * add parralel lint and test
    
    * add job artifact
    
    * Revert "add job artifact"
    
    This reverts commit 4d46054741a63fd6eaf1dd6345b3fbea0eca326f.
    
    * fix tests and release
    
    * add github bot login
    
    * update commit list
    
    * update name
    
    * fix react test

--- test/about.test.tsx
+++ test/about.test.tsx
@@ -1,11 +1,11 @@
 import { render } from '@testing-library/react';
-import React from 'react';
 import { About } from '../src/client/pages/About';
+import React from 'react';
 
 describe('About', () => {
   it('Should render author name', () => {
     const { container } = render(<About />);
 
-    expect(container.textContent).toBe('AboutAuthor: Anastasia');
+    expect(container.textContent).toBe('AboutAuthor: name');
   });
 });
