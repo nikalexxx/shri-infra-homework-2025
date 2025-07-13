dae957c627efd00503e7a48fd3e135a3b9222c1c
Bronina A.O <bronina04@mail.ru>
2025-07-13 18:32:15 +0300

    init: first commit

--- test/about.test.tsx
+++ test/about.test.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import { render } from '@testing-library/react';
+import React from 'react';
 import { About } from '../src/client/pages/About';
 
 describe('About', () => {
-    it('Should render author name', () => {
-        const { container } = render(<About />);
+  it('Should render author name', () => {
+    const { container } = render(<About />);
 
-        expect(container.textContent).toBe('AboutAuthor: [Your Name]');
-    });
+    expect(container.textContent).toBe('AboutAuthor: Anastasia');
+  });
 });
