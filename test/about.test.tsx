c9981fb5bea23d83104a118ee9ba727426811d8f
Anuar <ava200ava@gmail.com>
2025-07-14 01:14:37 +0500

    еуые

--- test/about.test.tsx
+++ test/about.test.tsx
@@ -5,7 +5,7 @@ import { About } from '../src/client/pages/About';
 describe('About', () => {
     it('Should render author name', () => {
         const { container } = render(<About />);
-
+1
         expect(container.textContent).toBe('AboutAuthor: Anuar');
     });
 });
