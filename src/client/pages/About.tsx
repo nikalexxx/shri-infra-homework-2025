6679bb0cda8c3c23f652df08d1e29008212726ea
Bronina A.O <bronina04@mail.ru>
2025-07-13 22:05:46 +0300

    edit about name

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -11,7 +11,7 @@ export const About: React.FC = () => {
       <div className="row">
         <div className="col">
           <h1>About</h1>
-          <p>Author: Egor Kreed</p>
+          <p>Author: Anasteisha</p>
         </div>
       </div>
     </div>
