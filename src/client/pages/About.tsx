0abd669d83c94e442ceddd0c761255c939a77f56
Bronina A.O <bronina04@mail.ru>
2025-07-13 21:19:13 +0300

    eto egor kreed

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -11,7 +11,7 @@ export const About: React.FC = () => {
       <div className="row">
         <div className="col">
           <h1>About</h1>
-          <p>Author: test100500</p>
+          <p>Author: Egor Kreed</p>
         </div>
       </div>
     </div>
--- test/about.test.tsx
+++ test/about.test.tsx
@@ -6,6 +6,6 @@ describe('About', () => {
   it('Should render author name', () => {
     const { container } = render(<About />);
 
-    expect(container.textContent).toBe('AboutAuthor: test100500');
+    expect(container.textContent).toBe('AboutAuthor: Egor Kreed');
   });
 });
