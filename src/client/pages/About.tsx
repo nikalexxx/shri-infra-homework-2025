0344783a35d4109d4596d46065e3e03712883d35
Torens-for-Yandex <crazytorens@gmail.com>
2025-07-17 18:12:14 +0300

    testReview

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -11,7 +11,7 @@ export const About: React.FC = () => {
       <div className="row">
         <div className="col">
           <h1>About</h1>
-          <p>Author: Anasteisha</p>
+          <p>Author: not Anasteisha</p>
         </div>
       </div>
     </div>
