c73110a8c673bf61d17fe1b6b48fd8e8b47f5b4e
Torens-for-Yandex <crazytorens@gmail.com>
2025-07-17 18:22:00 +0300

    review - change author

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -11,7 +11,7 @@ export const About: React.FC = () => {
       <div className="row">
         <div className="col">
           <h1>About</h1>
-          <p>Author: not Anasteisha</p>
+          <p>Author: Roman</p>
         </div>
       </div>
     </div>
