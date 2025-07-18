8720be85cd9dd11fa088072e2fc0fb0e0696dce6
YetAnotherKenshi <102178530+YetAnotherKenshi@users.noreply.github.com>
2025-07-18 22:27:46 +0300

    cross check commit 1

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -12,7 +12,7 @@ export const About: React.FC = () => {
         <div className="col">
           <h1>About</h1>
           <p>
-            Author: [Rakhov Roman]
+            Author: [Rakhov Roman
           </p>
         </div>
       </div>
