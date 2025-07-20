056fcc443d465071d410e95a0a1c2fa753f74069
Daniel <116294062+VBNetEnjoyer@users.noreply.github.com>
2025-07-20 13:54:37 +0300

    поломал

--- src/client/pages/About.tsx
+++ src/client/pages/About.tsx
@@ -5,17 +5,15 @@ import { cn } from "@bem-react/classname";
 const bem = cn("About");
 //Исправлено имя автора
 export const About: React.FC = () => {
-  return (
-    <div className={bem()}>
-      <Helmet title="About" />
-      <div className="row">
-        <div className="col">
-          <h1>About</h1>
-          <p>
-            Author: Мария Яковенко
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={bem()}>
+			<Helmet title="About" />
+			<div className="row">
+				<div className="col">
+					<h1>About</h1>
+					<p>Author:</p>
+				</div>
+			</div>
+		</div>
+	);
 };
